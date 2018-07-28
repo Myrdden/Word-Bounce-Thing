d7d971b1fc06942d7e6b5e1381123fa05deadfd2
Arii <Myrdden42@gmail.com>
2018-07-28 17:44:38 -0600

    featuring 100% more completion

--- wordbounce.hs
+++ wordbounce.hs
@@ -1,12 +1,13 @@
 import System.IO
 import Data.List
+import qualified Data.Char as C (toLower)
 import System.Random (randomRIO)
 
 separate :: String -> [String]
 separate x = case dropWhile (==',') x of
                  "" -> []
-                 word -> w : separate xs
-                     where (w, xs) = break (==',') word
+                 word -> x : separate xs
+                     where (x, xs) = break (==',') word
                      -- Thanks source!
 
 parse :: String -> [[String]]
@@ -15,7 +16,32 @@ parse = map separate . lines . filter (/='\r')
 select :: [a] -> IO a
 select xs = fmap (xs !!) $ randomRIO (0, length xs - 1)
 
-main = do
-    inFile <- readFile "something.txt"
+roll :: Int -> IO (Int, Int)
+roll x = do
+    a <- randomRIO (0, x - 1)
+    b <- randomRIO (0, x - 1)
+    if a == b then roll x else return (a,b)
 
+toLower :: String -> String
+toLower = map C.toLower
+
+guess :: [[String]] -> IO ()
+guess l@(x:xs) = do
+    word <- select xs
+    lang <- roll $ length word
+    let (a,b) = lang in do
+        putStr $ "Type \"" ++ (word !! a) ++ "\" in " ++ (x !! b) ++ ": "
+        hFlush stdout
+        input <- getLine
+        if input == "quit" then
+            return ()
+        else do
+            putStrLn $ if (toLower input) == (word !! b) then "Correct!" else "Incorrect."
+            guess l
+    return ()
+
+main = do
+    inFile <- readFile "dict.txt"
+    putStrLn "Type \"quit\" to exit.\n"
+    guess (parse inFile)
     return ()
